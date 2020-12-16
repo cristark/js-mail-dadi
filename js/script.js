cc08b96f78e7d6b3f68703026e8a06fff36b0142
Cristiano <cristiano.tarconi@gmail.com>
2020-12-16 22:05:20 +0100

    even or odd game js logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 /* Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
+
+// | GIOCO DEI DADI
 // Dichiaro le variabili principali
 var playerNumber, cpuNumber, risultato;
 
@@ -10,11 +12,9 @@ playButton.addEventListener("click", function() {
 
     //Inserire numero giocatore
     playerNumber = Math.floor(Math.random() * 6) + 1;
-    console.log(playerNumber);
 
     // Numero casuale generato dal computer
     cpuNumber = Math.floor(Math.random() * 6) + 1;
-    console.log(cpuNumber);
 
     var result = 'Parità! Tenta nuovamente la fortuna!';
 
@@ -30,3 +30,55 @@ playButton.addEventListener("click", function() {
     document.getElementById('cpu_dice').innerHTML = cpuNumber;
     document.getElementById('message').innerHTML = result;
 });
+
+
+// | GIOCO PARI O DISPARI
+var firstChoice = document.getElementById('option_choice').value;
+console.log('UTENTE SCEGLIE: ' + firstChoice);
+var secondChoice = document.getElementById('number_choice').value;
+console.log('UTENTE SCEGLIE: ' + secondChoice);
+
+//Stampo le scelte dell'utente all'interno del gioco
+var playButton = document.getElementById("stamp_choice");
+playButton.addEventListener("click", function() {
+
+    document.getElementById('opt_choice').innerHTML = firstChoice;
+    document.getElementById('numb_choice').innerHTML = secondChoice;
+
+});
+
+// Calcolo scelte del computer sulla base di quelle dell'utente
+var cpufirstChoice, cpusecondChoice;
+
+if (firstChoice == 'pari'){
+    cpufirstChoice = 'dispari';
+} else {
+    cpufirstChoice = 'pari';
+}
+
+cpusecondChoice = Math.floor(Math.random() * 6);
+
+console.log('CPU sceglie: ' + cpufirstChoice);
+console.log('CPU sceglie: ' + cpusecondChoice);
+
+//Calcolo l'esito della partita e stabilisco se il num. è pari o dispari
+var choiceResult = Number(secondChoice) + Number(cpusecondChoice);
+console.log('La somma è: ' + choiceResult);
+
+var catResult = choiceResult % 2;
+console.log(catResult);
+
+// Converto il risultato in un valore pari o dispari
+if (catResult == 0) {
+    choiceResult = 'pari';
+}   else {
+    choiceResult = 'dispari';
+}
+
+console.log('La somma è: ' + choiceResult);
+
+if (choiceResult == firstChoice) {
+    console.log('Hai vinto!');
+}   else {
+    console.log('Hai perso');
+}
